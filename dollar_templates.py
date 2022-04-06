ee21e740387ca16f6afd780f359e6d4df3b5e5fb
Frank Seifferth <frankseifferth@posteo.net>
2022-04-06 15:50:43 +0200

    Fix elsetree in If parsing

--- dollar_templates.py
+++ dollar_templates.py
@@ -152,7 +152,7 @@ def __parse_if(tokens: list[Token]) -> If:
         l.insert(0, MetaToken(end[4:]))
         elsetree, _ = __parse(l, endtokens=['endif'])
     else:
-        elsetree = PlainLeaf(PlainToken(''))
+        elsetree = Str(PlainToken(''))
     return If(condition, thentree, elsetree)
 def __parse_for(tokens: list[Token]) -> For:
     l = tokens
