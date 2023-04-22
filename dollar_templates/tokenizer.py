43905ae535faec2db6761c55909819b3b187c4e4
Frank Seifferth <frankseifferth@posteo.net>
2023-04-22 10:55:00 -0500

    Extract performance fix into list-splitting function

--- dollar_templates/tokenizer.py
+++ dollar_templates/tokenizer.py
@@ -23,30 +23,39 @@ def tokenize(template: str) -> list[Token]:
     meaning according to the template syntax, such as $var$, $if(var)$,
     $for(var)$, $endfor$ or $endif$.
     """
+    def split_list(l: list, char: str) -> tuple[list,list]:
+        """
+        Returns two lists. The first list is the one containing everything
+        up to (but excluding) char, the second one is possibly empty;
+        but if it is not empty, it holds char at index 0 as well as
+        whatever follows char in the original list.
+        """
+        try:
+            next = l.index(char)
+            part = l[:next]; l = l[next:]
+        except ValueError:
+            part = l; l = []
+        return part, l
     l = list(template)
     current = list()
     current_type = "plain"
     while len(l) > 0:
         if current_type == "plain":
-            try:
-                next_dollar = l.index('$')
-            except ValueError:
-                current.extend(l)
-                l = []; break
-            current.extend(l[:next_dollar])
-            l = l[next_dollar:]
+            head, l = split_list(l, '$')
+            current.extend(head)
+            if len(l) == 0: break
             if l[:2] == ['$','$']:          # Literal $
                 current.append("$"); l.pop(0); l.pop(0)
             elif l[:3] == ['$','-','-']:    # Line comment
                 while l[0] != '\n':
                     l.pop(0)
                 l.pop(0)
-            elif l[0] == '$' and l[1] == '{':
+            elif l[:2] == ['$', '{']:
                 l.pop(0); l.pop(0)
                 yield PlainToken("".join(current))
                 current = list()
                 current_type = "meta_curly"
-            elif l[0] == '$':
+            elif l[:1] == ['$']:
                 l.pop(0)
                 yield PlainToken("".join(current))
                 current = list()
