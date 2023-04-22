19dc57a01150ebb371d8476252089abd326f6138
Frank Seifferth <frankseifferth@posteo.net>
2023-04-22 16:24:13 -0500

    Bump version to 1.0.2.dev
    Having another look at semver.org, it seems that the `dev`-suffix
    is supposed to be used for denoting a pre-release. as such, it would
    be sorted before, not after the corresponding number. Hence, when
    bumping the version after a release, I should probably already bump
    the version number to the next release I anticipate when adding the
    `dev`-suffix. Also note that setuptools encourages the use of a dot
    as separator before the `dev` suffix, while it suggests using a minus
    sign for post-releases.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('readme.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='dollar_templates',
-    version='1.0.1-dev',
+    version='1.0.2.dev',
     packages=['dollar_templates'],
     python_requires='>=3.9',
     author='Frank Seifferth',
